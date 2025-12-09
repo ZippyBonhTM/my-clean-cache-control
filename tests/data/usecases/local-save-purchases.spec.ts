d8bf025c4b148b81b38d96c88881115b0bdfecde
ZippyBonhTM <zippybont@gmail.com>
2025-12-08 21:17:54 -0300

    add messages for CacheStoreApy and fix vitest path resolvers

--- tests/data/usecases/local-save-purchases.spec.ts
+++ tests/data/usecases/local-save-purchases.spec.ts
@@ -2,16 +2,20 @@ import { LocalSavePurchases } from "@/data/usecases/local-save-purchases.js";
 import type { CacheStore } from "@/domain/protocols/cache-store.js";
 import { describe, expect, test } from "vitest";
 
+enum CacheStoreCalls {
+  delete,
+  save,
+}
+
 class CacheStoreSpy implements CacheStore {
-  saveCallsCount: number = 0;
-  deleteCallsCount: number = 0;
+  messages: Array<CacheStoreCalls> = [];
 
   async save(): Promise<void> {
-    this.saveCallsCount++;
+    this.messages.push(CacheStoreCalls.save);
   }
-  
+
   async delete(): Promise<void> {
-    this.deleteCallsCount++;
+    this.messages.push(CacheStoreCalls.delete);
   }
 }
 
@@ -25,14 +29,12 @@ function SutFactory() {
 describe('LocalSavePurchases', () => {
   test('Shold not calls any method when initialized sut', () => {
     const { cacheStore } = SutFactory();
-    expect(cacheStore.saveCallsCount).toBe(0);
-    expect(cacheStore.deleteCallsCount).toBe(0);
+    expect(cacheStore.messages).toEqual([]);
   });
 
   test('Shold delete before save new cache', async () => {
     const { cacheStore, sut } = SutFactory();
     await sut.save();
-    expect(cacheStore.saveCallsCount).toBe(1);
-    expect(cacheStore.deleteCallsCount).toBe(1);
+    expect(cacheStore.messages).toEqual([CacheStoreCalls.delete, CacheStoreCalls.save]);
   });
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
-  test: {}
+  test: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  }
 });
