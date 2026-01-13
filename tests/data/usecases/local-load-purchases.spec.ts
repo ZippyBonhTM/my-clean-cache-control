4ecfb2bfabb93b2bc00bcbbaa074b2292e300a0d
ZippyBonhTM <zippybont@gmail.com>
2026-01-13 02:03:02 -0300

    Must load correctly

--- tests/data/usecases/local-load-purchases.spec.ts
+++ tests/data/usecases/local-load-purchases.spec.ts
@@ -1,7 +1,13 @@
-import CacheStoreSpy from "@tests/moks/cache/cache-store-spy.js";
+import CacheStoreSpy,{ CacheStoreCalls }  from "@tests/moks/cache/cache-store-spy.js";
 import { test, describe, expect } from "vitest";
 import { LocalLoadPurchases } from "@/data/usecases/local-load-purchases.js";
+import { LocalSavePurchases } from "@/data/usecases/local-save-purchases.js";
+import type { PurchasesModel } from "@/domain/models/purchases-model.js";
 
+const mockPurchases: PurchasesModel = [
+  { id: '1', title: 'Item A', price: 9.99, quantity: 1, date: new Date('2025-12-10T12:00:00.000Z') },
+  { id: '2', title: 'Item B', price: 19.5, quantity: 2, date: new Date('2025-12-09T15:30:00.000Z') },
+];
 
 describe('LocalLoadPurchases', () => {
   test('Should return null if nothing is in the key', async () => {
@@ -21,4 +27,16 @@ describe('LocalLoadPurchases', () => {
     expect(sutCall).rejects.toThrow();
     expect(cacheStore.messages).toEqual([]);
   });
+
+  test('Should load correctly', async () => {
+    const cacheStore = new CacheStoreSpy();
+    const sut = new LocalLoadPurchases(cacheStore);
+    const localSavePurchases = new LocalSavePurchases(cacheStore);
+    
+    await localSavePurchases.save('purchases', mockPurchases);
+    const result = await sut.load('purchases');
+
+    expect(cacheStore.messages).toEqual([CacheStoreCalls.delete, CacheStoreCalls.save, CacheStoreCalls.load]);
+    expect(result).toEqual(mockPurchases);
+  })
 });
